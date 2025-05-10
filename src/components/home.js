dfdf0d6c953cc73d308f253732f5bcfd457904f5
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-05-10 19:24:28 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,21 @@
+// src/components/Home.js
+import React from "react";
+import { Link } from "react-router-dom";
+import products from "../constants/product";
 
+const Home = () => {
+  return (
+    <div>
+      {products.map((item) => (
+        <Link key={item.id} to={`/products/${item.id}`}>
+          <div>
+            {item.name} <button className="btn">Buy</button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
+// Ensure this export is correct
