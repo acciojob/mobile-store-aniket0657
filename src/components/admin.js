a423a78f2e13a3b4e34e4430c46ccd0959c1d134
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-05-10 19:22:17 +0530

    Update admin.js

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,1 +1,22 @@
+import React from "react";
+import products from "../constants/product";  // Make sure the file path is correct
+import { Link } from "react-router-dom";
 
+function Admin() {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link to={`/admin/products/${product.id}`}>
+              {product.name} - ${product.price}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Admin;
